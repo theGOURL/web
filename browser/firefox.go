27d67cc4bb7d6735c4ddb4cd6a2c6b9b0d093d21
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-01-18 20:16:44 -0300

    Go mod Init Web

---
+++ browser/firefox.go
@@ -0,0 +1,10 @@
+package browser
+
+import (
+	"github.com/theGOURL/system/commands"
+)
+
+// this function start firefox
+func FIREFOX() {
+	commands.Execute("firefox")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/theGOURL/web
+
+go 1.19
+
+require github.com/theGOURL/system v0.0.0-20230118230546-65f7b1f03336 // indirect
